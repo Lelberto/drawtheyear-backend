e03d559ba6af986f01eb9acdec45a85b69a30f9b
Lelberto <jsurieux@gmail.com>
2022-05-05 20:09:20 +0200

    Test fix

--- src/users/user.controller.spec.ts
+++ src/users/user.controller.spec.ts
@@ -1,4 +1,6 @@
+import { ConfigService } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
+import { HateoasService } from '../hateoas/hateoas.service';
 import { UserController } from './user.controller';
 import { UserRepository } from './user.repository';
 import { UserService } from './user.service';
@@ -9,7 +11,7 @@ describe('UserController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UserController],
-      providers: [UserService, UserRepository]
+      providers: [UserService, UserRepository, HateoasService, ConfigService]
     }).compile();
 
     controller = module.get<UserController>(UserController);
