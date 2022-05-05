c781b7776ea3ebd91eeec0924ae2029aa97d9672
Lelberto <jsurieux@gmail.com>
2022-05-05 14:38:48 +0200

    HATEOAS implementation

--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -1,5 +1,7 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Req, UsePipes, ValidationPipe } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import { Request } from 'express';
+import { HateoasService } from 'src/hateoas/hateoas.service';
 import { IdToUserPipe } from './id-to-user.pipe';
 import { CreateUserDto, UpdateUserDto } from './user.dto';
 import { User } from './user.entity';
@@ -16,29 +18,53 @@ import { UserService } from './user.service';
 export class UserController {
 
   private readonly userService: UserService;
+  private readonly hateoas: HateoasService;
 
-  public constructor (userService: UserService) {
+  public constructor (userService: UserService, hateoas: HateoasService) {
     this.userService = userService;
+    this.hateoas = hateoas;
   }
 
   @Get()
   public async find() {
-    return await this.userService.find();
+    return { users: await this.userService.find() };
   }
 
   @Get(':id')
-  public findById(@Param('id', IdToUserPipe) user: User) {
-    return user;
+  public findById(@Req() req: Request, @Param('id', IdToUserPipe) user: User) {
+    return {
+      user,
+      links: [
+        this.hateoas.createLink(req, 'user-self', { userId: user.id }),
+        this.hateoas.createLink(req, 'user-emotions', { userId: user.id }),
+        this.hateoas.createLink(req, 'user-days', { userId: user.id })
+      ]
+    };
   }
 
   @Post()
-  public async create(@Body() body: CreateUserDto) {
-    return await this.userService.create(body);
+  public async create(@Req() req: Request, @Body() body: CreateUserDto) {
+    const user = await this.userService.create(body);
+    return {
+      user,
+      links: [
+        this.hateoas.createLink(req, 'user-self', { userId: user.id }),
+        this.hateoas.createLink(req, 'user-emotions', { userId: user.id }),
+        this.hateoas.createLink(req, 'user-days', { userId: user.id })
+      ]
+    };
   }
 
   @Patch(':id')
-  public async update(@Param('id') id: string, @Body() body: UpdateUserDto) {
+  public async update(@Req() req: Request, @Param('id') id: string, @Body() body: UpdateUserDto) {
     await this.userService.update(id, body);
+    return {
+      links: [
+        this.hateoas.createLink(req, 'user-self', { userId: id }),
+        this.hateoas.createLink(req, 'user-emotions', { userId: id }),
+        this.hateoas.createLink(req, 'user-days', { userId: id })
+      ]
+    };
   }
 
   @Delete(':id')
--- src/users/user.module.ts
+++ src/users/user.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { HateoasModule } from 'src/hateoas/hateoas.module';
 import { UserController } from './user.controller';
 import { User } from './user.entity';
 import { UserRepository } from './user.repository';
@@ -9,7 +10,10 @@ import { UserService } from './user.service';
  * User module
  */
 @Module({
-  imports: [TypeOrmModule.forFeature([User, UserRepository])],
+  imports: [
+    TypeOrmModule.forFeature([User, UserRepository]),
+    HateoasModule
+  ],
   providers: [UserService],
   controllers: [UserController]
 })
