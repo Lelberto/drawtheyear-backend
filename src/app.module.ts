60706cde22e3b7abd1169dc1a583f3462e337645
Lelberto <jsurieux@gmail.com>
2022-05-05 19:56:25 +0200

    Move TypeOrmModule initialization in a Database module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,17 @@
 import { MiddlewareConsumer, Module, NestModule, OnApplicationBootstrap } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import databaseConfig, { DatabaseConfig } from 'config/database';
+import databaseConfig from 'config/database';
 import globalConfig, { NodeEnv } from 'config/global';
 import loggingConfig, { LoggingConfig } from 'config/logging';
 import serverConfig from 'config/server';
 import * as morgan from 'morgan';
-import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
-import { Day } from './days/day.entity';
+import { DatabaseModule } from './database/database.module';
 import { DayModule } from './days/day.module';
-import { Emotion } from './emotions/emotion.entity';
 import { EmotionModule } from './emotions/emotion.module';
 import { HateoasModule } from './hateoas/hateoas.module';
 import { AccessLogger } from './logger/access.logger';
 import { AppLogger } from './logger/app.logger';
-import { DatabaseLogger } from './logger/database.logger';
 import { LoggerModule } from './logger/logger.module';
-import { User } from './users/user.entity';
 import { UserModule } from './users/user.module';
 
 /**
@@ -31,28 +26,7 @@ import { UserModule } from './users/user.module';
       envFilePath: ['.env.development', '.env'],
       load: [globalConfig, serverConfig, databaseConfig, loggingConfig]
     }),
-    TypeOrmModule.forRootAsync({
-      useFactory: async (configService: ConfigService, logger: DatabaseLogger) => {
-        const env = configService.get<NodeEnv>('env');
-        const config = configService.get<DatabaseConfig>('database');
-        return {
-          type: config.type as any,
-          host: config.url,
-          port: config.port,
-          username: config.user,
-          password: config.password,
-          database: config.name,
-          entities: [User, Emotion, Day],
-          namingStrategy: new SnakeNamingStrategy(),
-          synchronize: env !== 'production',
-          dropSchema: env === 'test',
-          maxQueryExecutionTime: 1000,
-          logger,
-          logging: env === 'development'
-        }
-      },
-      inject: [ConfigService, DatabaseLogger]
-    }),
+    DatabaseModule,
     LoggerModule,
     UserModule,
     EmotionModule,
---
+++ src/database/database.module.ts
@@ -0,0 +1,38 @@
+import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
+import { DatabaseConfig } from '../../config/database';
+import { NodeEnv } from '../../config/global';
+import { Day } from '../days/day.entity';
+import { Emotion } from '../emotions/emotion.entity';
+import { DatabaseLogger } from '../logger/database.logger';
+import { User } from '../users/user.entity';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: async (configService: ConfigService, logger: DatabaseLogger) => {
+        const env = configService.get<NodeEnv>('env');
+        const config = configService.get<DatabaseConfig>('database');
+        return {
+          type: config.type as any,
+          host: config.url,
+          port: config.port,
+          username: config.user,
+          password: config.password,
+          database: config.name,
+          entities: [User, Emotion, Day],
+          namingStrategy: new SnakeNamingStrategy(),
+          synchronize: env !== 'production',
+          dropSchema: env === 'test',
+          maxQueryExecutionTime: 1000,
+          logger,
+          logging: env === 'development'
+        }
+      },
+      inject: [ConfigService, DatabaseLogger]
+    })
+  ]
+})
+export class DatabaseModule {}
