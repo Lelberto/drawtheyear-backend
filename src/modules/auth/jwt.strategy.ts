25bf0268c40b0d6e034c949bfc7dcf8a2960f81b
Lelberto <jsurieux@gmail.com>
2022-05-22 16:07:45 +0200

    Update JWT strategy

--- src/modules/auth/jwt.strategy.ts
+++ src/modules/auth/jwt.strategy.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { AccessTokenPayload } from '../../utils/types';
+import { UserService } from '../users/user.service';
 
 /**
  * JWT passport strategy
@@ -8,15 +10,24 @@ import { ExtractJwt, Strategy } from 'passport-jwt';
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
 
-  public constructor() {
+  private readonly userService: UserService;
+
+  public constructor(userService: UserService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
       secretOrKey: process.env.AUTH_JWT_SECRET_KEY
     });
+    this.userService = userService;
   }
 
-  public async validate(payload: any) {
-    return { userId: payload.sub, email: payload.email };
+  /**
+   * Validates the strategy
+   * 
+   * @param payload Access token payload
+   * @returns Authenticated user
+   */
+  public async validate(payload: AccessTokenPayload) {
+    return await this.userService.findById(payload.sub);
   }
 }
