af7bd66e08913cd834f2f69769e7a5b06361ad4b
Lelberto <jsurieux@gmail.com>
2022-05-28 16:47:42 +0200

    Add pagination configuration

--- src/modules/config/server.ts
+++ src/modules/config/server.ts
@@ -2,10 +2,16 @@
 
 export type ServerConfig = {
   port: number;
+  pagination: {
+    maxLimit: number;
+  };
 }
 
 export default (): { server: ServerConfig } => ({
   server: {
-    port: parseInt(process.env.PORT, 10) || 3000
+    port: parseInt(process.env.PORT, 10) || 3000,
+    pagination: {
+      maxLimit: parseInt(process.env.PAGINATION_LIMIT_MAX, 10) || 10
+    }
   }
 });
