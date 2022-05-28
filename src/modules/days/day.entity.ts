cde99b6c7544dff3aa4baee642e5d94048f1a241
Lelberto <jsurieux@gmail.com>
2022-05-28 21:27:04 +0200

    Import fix

--- src/modules/days/day.entity.ts
+++ src/modules/days/day.entity.ts
@@ -1,6 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { Exclude } from 'class-transformer';
-import moment from 'moment';
+import * as moment from 'moment';
 import { Column, Entity, JoinTable, ManyToMany, ManyToOne, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 import { Attachment } from '../attachments/attachment.entity';
 import { Emotion } from '../emotions/emotion.entity';
