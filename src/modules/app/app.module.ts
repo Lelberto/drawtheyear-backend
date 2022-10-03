d6fd4aeba3c5a5874b783912866193f429793f1a
Lelberto <jsurieux@gmail.com>
2022-10-03 16:14:24 +0200

    Add pipe for resolving user from ID

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '../config/config.module';
 import { DatabaseModule } from '../database/database.module';
+import { EmotionModule } from '../emotions/emotion.module';
 import { UserModule } from '../users/user.module';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [ConfigModule, DatabaseModule, UserModule],
+  imports: [ConfigModule, DatabaseModule, UserModule, EmotionModule],
   controllers: [],
   providers: [AppService],
 })
---
+++ src/modules/users/pipes/resolve-user.pipe.ts
@@ -0,0 +1,17 @@
+import { ArgumentMetadata, Injectable, PipeTransform } from '@nestjs/common';
+import { User } from '../entities/user.entity';
+import { UserService } from '../user.service';
+
+@Injectable()
+export class ResolveUserPipe implements PipeTransform<string, Promise<User>> {
+
+  private readonly userService: UserService;
+
+  public constructor(userService: UserService) {
+    this.userService = userService;
+  }
+
+  public async transform(value: string, metadata: ArgumentMetadata): Promise<User> {
+    return await this.userService.findById(value);
+  }
+}
