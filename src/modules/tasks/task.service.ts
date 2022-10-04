e4da0f5a86dfcc3995d588df4fd21cf64f7a3c4a
Lelberto <jsurieux@gmail.com>
2022-10-04 16:52:33 +0200

    Fix context in jobs

--- src/modules/tasks/task.service.ts
+++ src/modules/tasks/task.service.ts
@@ -1,11 +1,11 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable, OnApplicationBootstrap } from '@nestjs/common';
 import { SchedulerRegistry } from '@nestjs/schedule';
 import { CronJob } from 'cron';
 import { Job } from './jobs/job';
 import { ResetUsernameChangeCountJob } from './jobs/reset-username-change-count.job';
 
 @Injectable()
-export class TaskService implements OnModuleInit {
+export class TaskService implements OnApplicationBootstrap {
 
   private readonly schedulerRegistry: SchedulerRegistry;
   private readonly jobs: Job[];
@@ -20,13 +20,13 @@ export class TaskService implements OnModuleInit {
     ]
   }
 
-  public async onModuleInit() {
+  public async onApplicationBootstrap() {
     await this.loadJobs();
   }
 
   private async loadJobs(): Promise<void> {
     this.jobs.forEach(job => {
-      const cronJob = new CronJob(job.time, job.execute);
+      const cronJob = new CronJob(job.time, () => job.execute());
       this.schedulerRegistry.addCronJob(job.name, cronJob);
       cronJob.start();
       console.log(`Loaded cron job ${job.name} (${job.time})`);
