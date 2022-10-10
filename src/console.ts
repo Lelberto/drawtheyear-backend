2bb8151d533b620d80f72df70a79d033875dece9
Lelberto <jsurieux@gmail.com>
2022-10-10 18:35:06 +0200

    Async fix

--- src/console.ts
+++ src/console.ts
@@ -4,7 +4,7 @@ import { CommandService } from './modules/commands/command.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(CommandModule);
-  app.get(CommandService).execute(process.argv);
+  await app.get(CommandService).execute(process.argv);
   await app.close();
 }
 bootstrap();
